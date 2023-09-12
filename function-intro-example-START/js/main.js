109ac4bcf528430a46db480697077ecc5296349f
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-12 16:42:28 -0600

    Not finished function class. Need to review it

--- function-intro-example-START/js/main.js
+++ function-intro-example-START/js/main.js
@@ -1,2 +1,37 @@
 // this is our javascript file.
+//make shure my javascript file is linked
+console.log("javascript is linked")
+
+const getUserName = () => {
+    let username = prompt("What is your user name?")
+    return username
+}
+
+const getDescription = () => {
+    let description = prompt("Enter a description")
+    return description
+}
+
+const updateDescription = () => {
+    // get the description
+    let newDescription = getDescription()
+    // update the description on the page
+    let descriptionElement = document.querySelector(".description")
+    descriptionElement.innerText = newDescription
+}
+
+// let updateUserName = document.querySelector("#username")
+// updateUserName.innerText = 'Viktor'
+
+const updateUserName = () => {
+    let username = getUserName()
+    let usernameElement = document.querySelector("#username")
+    usernameElement = document.querySelector(".description.lead")
+    descriptionElement.innerText = newDescription
+}
+
+const updateUserName = () => {
+    let username = getUserName()
+    let usernameElement = document.querySelector()
+}
 
