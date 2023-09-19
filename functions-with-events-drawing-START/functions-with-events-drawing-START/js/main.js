d1671f0b6c3164979fb07d39933c3d80ba06f633
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-19 15:38:28 -0600

    Added 3 projects

---
+++ functions-with-events-drawing-START/functions-with-events-drawing-START/js/main.js
@@ -0,0 +1,12 @@
+// this is our javascript file.
+
+
+// select the entire body and main of the document.
+
+// add an event listener to the body on the mousemove
+// select the green-box
+// modify the style attributes of the green-box of the
+// top and left with the position of the event.
+
+// add an event listener to click that will add a blue box to the main
+// at the position of the mouse.
