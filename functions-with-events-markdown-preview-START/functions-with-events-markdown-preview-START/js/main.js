9e5bcdbc7f613509767ad536aa85db2df1f05ced
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-19 16:33:27 -0600

    finished event listener in class

--- functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
+++ functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
@@ -8,14 +8,26 @@
 
 
 // select the text input. 
-
-
+let textInput = document.querySelector("#markdown-input-text")
+let timeExecuted = 0
+// console.log(textInput)
 // make an event listener that listens to keydown on the input
-    // call the render preview function
+  textInput.addEventListener("keyup", (event)=> {
+    console.log("We are in the event hendler")
+    //I want to get the value of this input
+    timeExecuted++;
+    console.log(event.targer.value) //is the same as textInput.value
+  
+// call the render preview function
+    renderPreview(event.target.value)
     // you'll see that the preview changes
     // every time we have something.
-
+})
 
 // create a function call renderPreview
+const renderPreview = (newInputValue) => {
     // select the mark down element
+    let preview = document.querySelector("#markdown-preview")
     // use the imported library called marked and parse the new input value.
+    preview.innerHTML = marked.parse(newInputValue)
+}
