eeb6e022c15b6ea81315d91077d5d4ed4a00e7ad
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-20 12:57:04 -0600

    Reviewed the class material and made some adjustments

--- functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
+++ functions-with-events-markdown-preview-START/functions-with-events-markdown-preview-START/js/main.js
@@ -5,29 +5,27 @@
 // marked js package and we can see it's available here
 // through the global scope of javascript.
 // more information see here: https://marked.js.org/
-
-
 // select the text input. 
 let textInput = document.querySelector("#markdown-input-text")
-let timeExecuted = 0
+
 // console.log(textInput)
-// make an event listener that listens to keydown on the input
-  textInput.addEventListener("keyup", (event)=> {
-    console.log("We are in the event hendler")
+// make an event listener that listens to keyup on the input
+textInput.addEventListener("keyup", (event) => {
+    console.log("We are in the event handler")
     //I want to get the value of this input
-    timeExecuted++;
-    console.log(event.targer.value) //is the same as textInput.value
-  
-// call the render preview function
-    renderPreview(event.target.value)
-    // you'll see that the preview changes
-    // every time we have something.
+    console.log(event.target.value)  //this is the same as textInput.value
+
+    // // call the render preview function
+        renderPreview(event.target.value)
+    //     // you'll see that the preview changes
+    //     // every time we have something.
 })
 
-// create a function call renderPreview
+// // create a function call renderPreview
 const renderPreview = (newInputValue) => {
     // select the mark down element
     let preview = document.querySelector("#markdown-preview")
     // use the imported library called marked and parse the new input value.
     preview.innerHTML = marked.parse(newInputValue)
 }
+//  let videoGameCheck = (game)
