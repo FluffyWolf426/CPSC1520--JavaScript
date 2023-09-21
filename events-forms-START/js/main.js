c3cf9a19c501a0e4022cdd25e9163b66da0831a2
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-21 10:02:49 -0600

    Froms in class not done jet. Need a review

--- events-forms-START/js/main.js
+++ events-forms-START/js/main.js
@@ -1,18 +1,31 @@
 // this is our javascript file.
 
+let budgetForm = document.querySelector("#budget-form")
+// console.log(budgetForm.)
 // intercept the form.
 
 // focus to the first element
+budgetForm.elements["budget-title"].focus()
 
 // create an event listener on the form.
+budgetForm.addEventListener("submit",(event) =>{
     // get the form element values for title, description and amount
+
+
     // prevent the form from being submitted.
+    event.preventDefault()
+    console.log("before")
+    let form = event.target //that is the same as budgetForm
+
+    let  title =form.elements["budget-title"]
+    let description = form.elements["budget-description"]
+    let amount = form.elements["amount"]
     // get the form values.
     // add the item
     // update current budget total
     // reset the values
     // focus back at the title
-
+})
 
 // add a budget item create function
 /*
@@ -20,9 +33,14 @@
     TITLE HERE (AMOUNT HERE) - DESCRIPTION HERE
 </li>
 */
+const addEventListener = (title,amount,description) => {
+let budgetList = documetn.querySelector(".current-budget")
+budgetList.innerHTML += `<li class="list-group-item list-group-item-action" aria-current="true">
+${title}(AMOUNT HERE) - DESCRIPTION HERE
+</li>`
 
+}
 // add a updateTotal function
-
 
 // add active class to list item on mouseover
 
