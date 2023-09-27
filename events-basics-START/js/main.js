c7b7ec6bd35c75eb9fef854ed2bdc2b4dc661e8d
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-27 08:25:21 -0600

    yop

--- events-basics-START/js/main.js
+++ events-basics-START/js/main.js
@@ -14,7 +14,7 @@ console.log(clicked)
 
 //override default behaviour
 
-let naitLink = document.querySelector(".link")
+let naitLink = document.querySelector("#link")
 
 naitLink.addEventListener("click", (event) => {
     //we dont want to leave our page
@@ -25,4 +25,23 @@ naitLink.addEventListener("click", (event) => {
 
 //how to start the form from submitting
 
+let form = document.querySelector(".form")
 
+form.addEventListener("submit", (event) => {
+   //prevent the default action form happening
+     event.preventDefault()
+})
+
+//part 3 
+
+let colorDiv = document.querySelector(".colours")
+
+colorDiv.addEventListener("click", (event) =>{
+    //here we're going to see that the event.target is the most specific element that is being clicked
+    console.log(event.target)
+    console.log(event.target.className)
+  //we can reset the background color of the page.
+
+  document.body.style = "background: " + event.target.className
+  colorDiv.style = "background: " + event.target.className
+ })
