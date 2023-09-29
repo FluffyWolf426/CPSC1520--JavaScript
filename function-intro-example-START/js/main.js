20a4b6eac36e8722713218a28a258e3f1d55f7c9
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-09-29 10:50:44 -0600

    om

--- function-intro-example-START/js/main.js
+++ function-intro-example-START/js/main.js
@@ -1,39 +1,39 @@
 // this is our javascript file.
-//make shure my javascript file is linked
-// console.log("javascript is linked")
+// make shure my javascript file is linked
+console.log("javascript is linked")
 
-// const getUserName = () => {
-//     let username = prompt("What is your user name?")
-//     return username
-// }
+const getUserName = () => {
+    let username = prompt("What is your user name?")
+    return username
+}
 
-// const getDescription = () => {
-//     let description = prompt("Enter a description")
-//     return description
-// }
+const getDescription = () => {
+    let description = prompt("Enter a description")
+    return description
+}
 
-// const updateDescription = () => {
-//     // get the description
-//     let newDescription = getDescription()
-//     // update the description on the page
-//     let descriptionElement = document.querySelector(".description")
-//     descriptionElement.innerText = newDescription
-// }
+const updateDescription = () => {
+    // get the description
+    let newDescription = getDescription()
+    // update the description on the page
+    let descriptionElement = document.querySelector(".description")
+    descriptionElement.innerText = newDescription
+}
 
-// // let updateUserName = document.querySelector("#username")
-// // updateUserName.innerText = 'Viktor'
+// let updateUserName = document.querySelector("#username")
+// updateUserName.innerText = 'Viktor'
 
-// const updateUserName = () => {
-//     let username = getUserName()
-//     let usernameElement = document.querySelector("#username")
-//     usernameElement = document.querySelector(".description.lead")
-//     descriptionElement.innerText = newDescription
-// }
+const updateUserName = () => {
+    let username = getUserName()
+    let usernameElement = document.querySelector("#username")
+    usernameElement = document.querySelector(".description.lead")
+    descriptionElement.innerText = newDescription
+}
 
-// const updateUserName = () => {
-//     let username = getUserName()
-//     let usernameElement = document.querySelector()
-// }
+const updateUserName = () => {
+    let username = getUserName()
+    let usernameElement = document.querySelector()
+}
 let name = "Viktor"
 const loversCats = funtcion (name) {
     let catLover = name + " loves cats"
